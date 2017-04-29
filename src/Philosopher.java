34c82291b597e176e44f26cf332bff155b02f617
garrettluskey <garrett.luskey@gmail.com>
2017-04-29 16:06:30 -0500

    all but comments done

--- src/Philosopher.java
+++ src/Philosopher.java
@@ -1,37 +1,18 @@
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Philosopher extends Thread 
 { 
    // each Philosopher is assigned an integer ID and two forks 
    private int ID = 0;
    private static int IDCounter = 0;
-   public enum States{THINKING, EATING,WAIT_LEFT_FORK, WAIT_RIGHT_FORK};
    private static int numberOfPhilosophers = 5;
    private static Fork[] forks = new Fork[numberOfPhilosophers];
-   private int thinking = 0;
-   private int eating = 0;
-   
-
-   Philosopher()
-   {
-      
-   }
-
-   public void curState(States curState)
-   {
-      switch(curState)
-      {
-         case THINKING:
-            break;
-         case EATING:
-            break;
-         case WAIT_LEFT_FORK:
-            break;
-         default://default WAIT_RIGHT_FORK
-            break;
-      }
-   }
+   private int thinkingTime = 0;
+   private int eatingTime = 0;
+   private int eatingAmount = 0;
+   private int thinkingAmount = 0;
    
 
     public void run()  
@@ -40,12 +21,13 @@ public class Philosopher extends Thread
     boolean timeToGO = false;
     while (!timeToGO) 
     { 
-        System.out.println (ID + "THINKING"); 
+        System.out.println (ID + " THINKING"); 
         // TODO: think for random number of milliseconds <=1 sec 
         Random rand = new Random();
         try{
             int wait = rand.nextInt(999) + 1;
-            thinking += wait;
+            thinkingTime += wait;
+            thinkingAmount++;
             Thread.sleep(wait);
         } catch(InterruptedException e) {}
         
@@ -67,32 +49,36 @@ public class Philosopher extends Thread
         // TODO: eat for random number of milliseconds <= 1sec 
         try{
             int wait = rand.nextInt(999) + 1;
-            eating += wait;
+            eatingTime += wait;
+            eatingAmount++;
             Thread.sleep(wait);
         } catch(InterruptedException e) {}
         // TODO: release the fork on the left  
         forks[ID].setDown();
         // TODO: release the fork on the right
-        System.out.println (ID + "EATING");
+        System.out.println (ID + " EATING");
         if(ID == numberOfPhilosophers - 1){
             forks[0].setDown();
         } else { forks[ID + 1].setDown();}
-        if(thinking >= 1000){
+        if(thinkingTime >= 1000){
             timeToGO = !timeToGO;
         }
    } 
 
-   System.out.println(ID+ "DONE");
+   System.out.println(ID+ " DONE");
+   System.out.println("Philosopher " + (ID + 1) + " ate " + eatingAmount + " times for " + eatingTime + "ms");
+   System.out.println("They thought " + thinkingAmount + " times for " + thinkingTime + "ms");
    } 
 
    public static void main(String[] args)
    {
+       System.out.println(numberOfPhilosophers);
        for (int i = 0; i < numberOfPhilosophers; i++){
            forks[i] = new Fork();
        }
        Thread[] philosophers = new Thread[numberOfPhilosophers];
        for (int i = 0; i < numberOfPhilosophers; i++){
-           System.out.println ("start");
+           
            philosophers[i] = new Thread(new Philosopher());
            philosophers[i].start();
        }
