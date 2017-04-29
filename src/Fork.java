b9440d7132646846217f90f84d68aa84f5f67d6b
Justin Mulrooney <mulrooneyju@uwplatt.edu>
2017-04-29 16:21:36 -0500

    I deleted it all and submitted nothing

--- src/Fork.java
+++ src/Fork.java
@@ -1,22 +1,31 @@
+//
+//Names: Garrett Luskey and Justin Mulrooney
+//
+//Program 4
+//CAOS
 
+
+
+//This is the fork class. It represents the fork
 public class Fork 
 {
    private boolean beingHeld;
+   //This is the default constructor
    public Fork()
    {
       beingHeld = false;
    }
-   
+   //Tells if the fork is being held
    public boolean isHeld()
    {
       return beingHeld;
    }
-
+   //Tells the fork to be set down
    public void setDown()
    {
       beingHeld = false;
    }
-   
+   //Tells the fork to be set down
    public void pickUp()
    {
       beingHeld = true;
--- src/Philosopher.java
+++ src/Philosopher.java
@@ -1,7 +1,11 @@
-
+//Names: Garrett Luskey and Justin Mulrooney
+//
+//Program 4
+//Class: CAOS
+//
 import java.util.Random;
-import java.util.Scanner;
 
+//This is the philosopher class. It runs the program and makes the philosophers
 public class Philosopher extends Thread 
 { 
    // each Philosopher is assigned an integer ID and two forks 
@@ -14,62 +18,63 @@ public class Philosopher extends Thread
    private int eatingAmount = 0;
    private int thinkingAmount = 0;
    
-
+   //This method tells each philosopher thread to eat, think, and wait
+   //it also outputs the corresponding information at runtime of the thtreads
     public void run()  
     {
-        ID = IDCounter++;
-    boolean timeToGO = false;
-    while (!timeToGO) 
-    { 
-        System.out.println (ID + " THINKING"); 
-        // TODO: think for random number of milliseconds <=1 sec 
-        Random rand = new Random();
-        try{
-            int wait = rand.nextInt(999) + 1;
-            thinkingTime += wait;
-            thinkingAmount++;
-            Thread.sleep(wait);
-        } catch(InterruptedException e) {}
-        
-        if(forks[ID].isHeld() == false){
-            forks[ID].pickUp();
-            if (ID == numberOfPhilosophers - 1){
-                if (forks[0].isHeld() == false) {
-                    forks[0].pickUp();            
-                }
-                else { forks[ID].setDown(); }
-            }
-            else if( forks[ID + 1].isHeld() == false) {
-                forks[ID + 1].pickUp();
-            }
-            else { forks[ID].setDown(); }
-        }
+      ID = IDCounter++;
+      boolean timeToGO = false;
+      while (!timeToGO) 
+      { 
+          System.out.println (ID + " THINKING"); 
+          // TODO: think for random number of milliseconds <=1 sec 
+          Random rand = new Random();
+          try{
+              int wait = rand.nextInt(999) + 1;
+              thinkingTime += wait;
+              thinkingAmount++;
+              Thread.sleep(wait);
+          } catch(InterruptedException e) {}
 
-        
-        // TODO: eat for random number of milliseconds <= 1sec 
-        try{
-            int wait = rand.nextInt(999) + 1;
-            eatingTime += wait;
-            eatingAmount++;
-            Thread.sleep(wait);
-        } catch(InterruptedException e) {}
-        // TODO: release the fork on the left  
-        forks[ID].setDown();
-        // TODO: release the fork on the right
-        System.out.println (ID + " EATING");
-        if(ID == numberOfPhilosophers - 1){
-            forks[0].setDown();
-        } else { forks[ID + 1].setDown();}
-        if(thinkingTime >= 1000){
-            timeToGO = !timeToGO;
-        }
+          if(forks[ID].isHeld() == false){
+              forks[ID].pickUp();
+              if (ID == numberOfPhilosophers - 1){
+                  if (forks[0].isHeld() == false) {
+                      forks[0].pickUp();            
+                  }
+                  else { forks[ID].setDown(); }
+              }
+              else if( forks[ID + 1].isHeld() == false) {
+                  forks[ID + 1].pickUp();
+              }
+              else { forks[ID].setDown(); }
+          }
+
+
+          // TODO: eat for random number of milliseconds <= 1sec 
+          try{
+              int wait = rand.nextInt(999) + 1;
+              eatingTime += wait;
+              eatingAmount++;
+              Thread.sleep(wait);
+          } catch(InterruptedException e) {}
+          // TODO: release the fork on the left  
+          forks[ID].setDown();
+          // TODO: release the fork on the right
+          System.out.println (ID + " EATING");
+          if(ID == numberOfPhilosophers - 1){
+              forks[0].setDown();
+          } else { forks[ID + 1].setDown();}
+          if(thinkingTime >= 1000){
+              timeToGO = !timeToGO;
+          }
+     } 
+
+     System.out.println(ID+ " DONE");
+     System.out.println("Philosopher " + (ID + 1) + " ate " + eatingAmount + " times for " + eatingTime + "ms");
+     System.out.println("They thought " + thinkingAmount + " times for " + thinkingTime + "ms");
    } 
-
-   System.out.println(ID+ " DONE");
-   System.out.println("Philosopher " + (ID + 1) + " ate " + eatingAmount + " times for " + eatingTime + "ms");
-   System.out.println("They thought " + thinkingAmount + " times for " + thinkingTime + "ms");
-   } 
-
+    //This is the main method. It creats the threads and creats the forks
    public static void main(String[] args)
    {
        System.out.println(numberOfPhilosophers);
